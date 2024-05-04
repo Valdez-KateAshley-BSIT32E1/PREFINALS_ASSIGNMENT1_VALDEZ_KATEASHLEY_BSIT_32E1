4d7f7e8bc1f0af95f788a81d79f848549f34a349
valdez-kateashley-BSIT32E1 <160818011+Valdez-KateAshley-BSIT32E1@users.noreply.github.com>
2024-05-04 09:15:10 +0800

    Add IUserService

---
+++ PREFINALS_AUTH_VALDEZ_KATEASHLEY/AuthServer/Core/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AuthServer.Core
+{
+
+    public interface IUserService
+    {
+        User GetUserByUsername(string username);
+        void CreateUser(string username, string passwordHash);
+    }
+}
--- PREFINALS_AUTH_VALDEZ_KATEASHLEY/AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
+++ PREFINALS_AUTH_VALDEZ_KATEASHLEY/AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce37fa2fd652eeb337c1ba1f6e5db739f7860070")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f8f69853c58e2b13c8b8dc6ba5fb3b42fa452f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
