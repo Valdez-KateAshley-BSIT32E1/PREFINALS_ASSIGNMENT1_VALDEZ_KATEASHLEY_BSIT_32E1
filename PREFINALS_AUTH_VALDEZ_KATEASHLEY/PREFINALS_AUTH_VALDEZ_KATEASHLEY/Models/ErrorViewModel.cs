fa0b9257c2609fa318eb6683d128d76b26742c30
valdez-kateashley-BSIT32E1 <160818011+Valdez-KateAshley-BSIT32E1@users.noreply.github.com>
2024-05-04 08:33:00 +0800

    Set Up

---
+++ PREFINALS_AUTH_VALDEZ_KATEASHLEY/PREFINALS_AUTH_VALDEZ_KATEASHLEY/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PREFINALS_AUTH_VALDEZ_KATEASHLEY.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
