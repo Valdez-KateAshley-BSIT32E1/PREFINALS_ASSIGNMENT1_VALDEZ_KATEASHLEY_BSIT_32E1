7f8f69853c58e2b13c8b8dc6ba5fb3b42fa452f6
valdez-kateashley-BSIT32E1 <160818011+Valdez-KateAshley-BSIT32E1@users.noreply.github.com>
2024-05-04 09:12:34 +0800

    Add user

--- PREFINALS_AUTH_VALDEZ_KATEASHLEY/AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
+++ PREFINALS_AUTH_VALDEZ_KATEASHLEY/AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c067c5ae685a0f1026e5e1f7eae9c641b0a2b05b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce37fa2fd652eeb337c1ba1f6e5db739f7860070")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
